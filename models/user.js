04220a8718b979bbcdd386d4b97735edcbffe858
Alex Verkest <31321596+iAmDragyn@users.noreply.github.com>
2021-03-31 19:26:44 -0600

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose"),
     userSchema = mongoose.Schema({
-        name: String,
+        firstName: String,
+        lastName: String,
+        dateOfBirth: Date,
+        username: String,
         email: String,
-        zipCode: Number
+        password: String,
+        securityQuestion1: String,
+        securityAnswer1: String,
+        securityQuestion2: String,
+        securityAnswer2: String,
+        securityQuestion3: String,
+        securityAnswer3: String,
     });
 
 module.exports = mongoose.model("User", userSchema);
