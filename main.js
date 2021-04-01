1528cfed9ac327ad83b1199501c25badedc5e666
Alex Verkest <31321596+iAmDragyn@users.noreply.github.com>
2021-03-31 19:23:05 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,10 @@ const express = require("express"), app = express(),
 homeController = require("./controllers/homeController"),
 errorController = require("./controllers/errorController"),
 usersController = require("./controllers/usersController"),
-layouts = require("express-ejs-layouts");
+layouts = require("express-ejs-layouts"), mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/lets_take_a_selfie", 
+    {useNewUrlParser: true});
 
 app.set("port", process.env.PORT || 3000);
 
@@ -12,11 +15,7 @@ app.use(layouts);
 app.get("/", homeController.showIndex);
 
 app.use(express.static("public"))
-app.use(
-    express.urlencoded({
-        extended: false
-    })
-);
+app.use(express.urlencoded({extended: false}));
 
 app.use(express.json());
 
@@ -24,9 +23,9 @@ app.get("/thing", homeController.showThing);
 app.post("/signUp", homeController.postedSignUpForm);
 app.post("/logIn", homeController.postedLogInForm);
 
-app.get("/search", homeController.showResults);
-app.get("/settings", homeController.showSettings);
-app.get("/account", homeController.showAccount);
+// app.get("/search", homeController.showResults);
+// app.get("/settings", homeController.showSettings);
+// app.get("/account", homeController.showAccount);
 
 
 app.listen(app.get("port"), () => {
